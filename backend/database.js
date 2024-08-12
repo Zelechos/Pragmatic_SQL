3337b22492dca426754240c9b57a7ee3e6592faf
Alex T.H. <alex@inversionesbloom.com>
2024-08-12 12:36:32 -0400

    feat(v0.0.0): update the repo

--- backend/database.js
+++
@@ -1,9 +0,0 @@
-// importamos mongoose
-const mongoose = require('mongoose');
-
-// Nos conectamos a nuestro base de datos
-mongoose.connect(process.env.MONGODB_URI,{
-    useNewUrlParser: true
-})
-    .then(database => console.log("database connected"))
-    .catch(error => console.error(error));
