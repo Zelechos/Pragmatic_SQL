ce379cc90143106f09fb5c8f59c738496221da3d
Alex T.H. <zelechos@hotmail.com>
2024-08-20 17:21:36 -0400

    feat(v0.0.2): add statements sql

---
+++ Statements/distinct.sql
@@ -0,0 +1,11 @@
+# To select one distinct row of table (because all rows are distincts show all table)
+SELECT DISTINCT * FROM hello_sql.users;
+
+# To select distinct ages of table
+SELECT DISTINCT age FROM hello_sql.users;
+
+# To select distinct name of table
+SELECT DISTINCT name FROM hello_sql.users;
+
+# To select distinct multiple values of tables
+SELECT DISTINCT age, name FROM hello_sql.users;
---
+++ Statements/select.sql
@@ -0,0 +1,14 @@
+# To select all columns in table
+SELECT * FROM hello_sql.users;
+
+# To select one column in table
+SELECT name FROM hello_sql.users;
+
+# To select multiple columns in table
+SELECT user_id, name FROM hello_sql.users;
+
+# Example I
+SELECT email, password FROM hello_sql.users;
+
+# Example II
+SELECT name, surname, age, init_date FROM hello_sql.users;
